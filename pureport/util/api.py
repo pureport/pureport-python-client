e4d1b4503ba2dc2f26096e067a3b7f313aa34a1e
Doug Mittendorf <dmittendorf@patternhealthtech.com>
2019-05-24 15:21:33 -0400

    Use refresh_token to auto refresh the access_token if/when it's expired.

--- pureport/util/api.py
+++ pureport/util/api.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import time
 from requests import Session
 
 from ..exception.api import raise_response_exception
@@ -33,11 +34,49 @@ class RaiseForStatusSession(Session):
         return response
 
 
-class RelativeRaiseForStatusSession(RelativeSession, RaiseForStatusSession):
+class PureportSession(RelativeSession, RaiseForStatusSession):
     def __init__(self, *args, **kwargs):
         """
         A :class:`requests.Session` subclass that includes functionality from
         both :class:`RelativeSession` and :class:`RaiseForStatusSession`
         :param str base_url: the base url
         """
-        super(RelativeRaiseForStatusSession, self).__init__(*args, **kwargs)
+        super(PureportSession, self).__init__(*args, **kwargs)
+
+        self._access_token = None
+        self._refresh_token = None
+        self._token_expire_time = None
+
+    def login(self, key, secret):
+        """
+        Login to Pureport ReST API with the specified key and secret
+        This stores the access token on this client instance's session for usage.
+        :param str key: the key to use for login
+        :param str secret: the secret to user for login
+        :returns: the obtained access_token
+        :rtype: str
+        :raises: .exception.ClientHttpException
+        :raises: .exception.MissingAccessTokenException
+        """
+        result = self.post('/login', json={'key': key, 'secret': secret}).json()
+        self.set_access_token(result['access_token'])
+        self._refresh_token = result['refresh_token']
+        self._token_expire_time = time.time() + result['expires_in']
+
+        return self._access_token
+
+    def set_access_token(self, access_token):
+        self._access_token = access_token
+        self.headers.update({'Authorization': 'Bearer %s' % access_token})
+
+    def request(self, method, url, **kwargs):
+        if self._refresh_token and self._token_expire_time and time.time() > self._token_expire_time:
+            # Automatically refresh the access token using the refresh token
+            self._access_token = None
+            self._token_expire_time = None
+            del self.headers['Authorization']
+            result = self.post('/login/refresh', json={'refreshToken': self._refresh_token}).json()
+            self.set_access_token(result['access_token'])
+            self._token_expire_time = time.time() + result['expires_in']
+
+        return super(PureportSession, self).request(method, url, **kwargs)
