927720c7bba091e33bbbac8d26460c3429841f88
mtraynham <matt.traynham@pureport.com>
2020-03-30 13:50:13 -0400

    Bump develop to 1.0.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_requirements(file_name):
 
 setup(
     name='pureport-client',
-    version='1.0.3',
+    version='1.0.4',
     author='Pureport',
     author_email='noreply@pureport.com',
     license='MIT',
