fe9674420278e5c4d0a3c85fc5ab77c1e231e36b
mtraynham <matt.traynham@pureport.com>
2020-05-01 15:29:34 -0400

    Allow timestamps to parsed as dates

--- pureport/util/date.py
+++ pureport/util/date.py
@@ -8,7 +8,7 @@ def format_date(value):
     """
     Formats a datetime, date or string as an ISO-8601 string,
     2020-01-01T00:00:00.00000Z
-    :param datetime|date|str value:
+    :param datetime|date|int|str value:
     :rtype: str|None
     :raises: ValueError
     """
@@ -16,13 +16,17 @@ def format_date(value):
         return value.strftime(SERVER_DATE_FORMAT)
     elif isinstance(value, date):
         return value.strftime(SERVER_DATE_FORMAT)
+    elif (isinstance(value, int) or
+          isinstance(value, float)):
+        return datetime \
+            .fromtimestamp(value) \
+            .strftime(SERVER_DATE_FORMAT)
     elif isinstance(value, str):
         for fmt in [
             '%Y/%m/%d',
             '%Y-%m-%d',
             '%Y.%m.%d',
             '%Y,%m,%d',
-            '%Y\%m\%d',
             '%Y-%m-%dT%H',
             '%Y-%m-%dT%H:%M',
             '%Y-%m-%dT%H:%M:%S',
