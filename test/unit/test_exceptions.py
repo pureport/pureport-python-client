81d032b7351a1332edb703d44e76f90cacb87d8a
Peter Sprygada <privateip@users.noreply.github.com>
2020-08-23 09:29:54 -0400

    refactor the exceptions module (#46)
    This commit refactors the exceptions modules to simplify the interface
    and number of support exception classes.  It introduces an exception
    hierarchy with all exceptions now subclasses of `PureportClientError`.
    
    The changes in this commit also remove generating status specific
    exceptions based on http status code in lieu of a more streamlined
    single exception `ClientHttpError` that contains the status code and
    reason.
    
    Signed-off-by: Peter Sprygada <peter.sprygada@pureport.com>

---
+++ test/unit/test_exceptions.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*_
+#
+# Copyright (c) 2020, Pureport, Inc.
+# All Rights Reserved
+
+from unittest.mock import MagicMock
+
+import pureport_client.exceptions as exceptions
+
+from ..utils import utils
+
+
+def test_pureport_client_error():
+    message = utils.random_string()
+    obj = MagicMock()
+
+    exc = exceptions.PureportClientError(message, exc=obj)
+
+    assert exc.message == message
+    assert exc.exc == obj
+
+
+def test_pureport_client_connection_error():
+    message = utils.random_string()
+    obj = MagicMock()
+    conn = MagicMock()
+
+    exc = exceptions.PureportClientConnectionError(message, obj, connection=conn)
+
+    assert isinstance(exc, exceptions.PureportClientError)
+    assert exc.message == message
+    assert exc.connection == conn
+    assert exc.exc == obj
+
+
+def test_missing_action_token_exceptiion():
+    message = utils.random_string()
+    exc = exceptions.MissingAccessTokenError(message)
+    assert isinstance(exc, exceptions.PureportClientError)
+    assert exc.message == message
+
+
+def test_connection_operation_timeout_exception():
+    message = utils.random_string()
+    exc = exceptions.ConnectionOperationTimeoutError(message)
+    assert isinstance(exc, exceptions.PureportClientError)
+    assert isinstance(exc, exceptions.PureportClientConnectionError)
+    assert exc.message == message
+
+
+def test_connection_operation_failed_exception():
+    message = utils.random_string()
+    exc = exceptions.ConnectionOperationFailedError(message)
+    assert isinstance(exc, exceptions.PureportClientError)
+    assert isinstance(exc, exceptions.PureportClientConnectionError)
+    assert exc.message == message
+
+
+def test_client_http_exception():
+    message = utils.random_string()
+    status_code = utils.random_int()
+    reason = utils.random_string()
+    exc = exceptions.ClientHttpError(status_code, reason, message)
+    assert isinstance(exc, exceptions.PureportClientError)
+    assert exc.message == message
+    assert exc.status_code == status_code
+    assert exc.reason == reason
